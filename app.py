ed2f78bd93537c2c5cca13f2751b42f59b099548
arisuryana <suryanaputu192@gmail.com>
2019-10-14 21:32:47 +0800

    Initial Commit

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
 import os
+import pymysql.cursors
 import json
 import dialogflow_v2
 
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 5000))
 
-# def results():
-#     req = request.get_json()
-#     intentName = req.get('intent').get('displayName')
-#     action = req.get('queryResult').get('queryText')
-#     # coba = req.get('originalDetectIntentRequest').get('payload').get('https://api.line.me/v2/profile')
-#
-#     return {'fulfillmentText': action}
+connection = pymysql.connect(host='db4free.net',
+                             user='arisuryana',
+                             password='arisuryana',
+                             db='db_simak',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
 @app.route('/', methods=['POST'])
@@ -20,7 +20,31 @@ def webhook():
     data = request.get_json()
     intent_name = data.get("queryResult").get("intent").get("displayName")
     print(data)
+
+    if intent_name == 'Awal':
+        return Awal(data)
+
     return jsonify(request.get_json())
+
+
+def Awal(data):
+    cekUserID = data.get("originalDetectIntentRequest").get("payload").get("data").get("source").get("userId")
+
+    try:
+        result = None
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM tb_profile WHERE tb_profile.userID = %s"
+            cursor.execute(sql, (cekUserID))
+            result = cursor.fetchone()
+
+        print(result)
+        print(result['nama'])
+
+        return jsonify({'fulfillmentText': result['nama']})
+
+    finally:
+        connection.close()
+
 
 
 if __name__ == '__main__':
