5d72599978d685f9fbb020ba571addd04b6984d1
arisuryana <suryanaputu192@gmail.com>
2019-10-17 02:13:44 +0800

    Initial Commit

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def cekProfil(data):
             hasil = cursor.fetchone()
 
         response = {
-            'fulfillmentText': "NIM : {}\nNama : {}\nAlamat : {}\nJurusan : {}".format(hasil['nim'], hasil['nama'], hasil['alamat'], hasil['jurusan'])
+            'fulfillmentText': "NIM : {}\nNama : {}\nJenis Kelamin : {}\nAlamat : {}\nJurusan : {}".format(hasil['nim'], hasil['nama'], hasil['jenis_kelamin'], hasil['alamat'], hasil['jurusan'])
         }
 
         return jsonify(response)
